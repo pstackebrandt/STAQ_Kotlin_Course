a95049d2dd4e75478584d89e2eb637e9a1a29798
Peter Stackebrandt <p.stackebrandt@immowelt.de>
2019-07-01 17:41:50 +0200

    Solve 1 training unit

--- Small Exercises/Basics/Imports/src/Task.kt
+++ Small Exercises/Basics/Imports/src/Task.kt
@@ -1,9 +1,9 @@
 import domain.internal.Customer
+import domain.external.Customer as ExternalCustomer
 
 open class CustomerMapper {
-
-//implement here the solution
-
+    fun mapToInternalCustomer(customer: ExternalCustomer): Customer =
+            Customer(customer.name, customer.id)
 }
 
 fun main(args: Array<String>) {
