a453ebd61bec0135c240431121c8a1256f0c7d99
Peter Stackebrandt <p.stackebrandt@immowelt.de>
2019-07-01 17:07:01 +0200

    Solve 1 training unit

--- Small Exercises/Basics/Classes - fields/src/Task.kt
+++ Small Exercises/Basics/Classes - fields/src/Task.kt
@@ -1,4 +1,18 @@
-//Create here your classes
+open class Point(val name: String)
+{
+    open fun nrOfCorners() = 0
+
+}
+
+class Line(name: String):Point(name)
+{
+    override fun nrOfCorners() = 2
+}
+
+class Square(name: String):Point(name)
+{
+    override fun nrOfCorners()= 4
+}
 
 /**
  * Main method to play with
