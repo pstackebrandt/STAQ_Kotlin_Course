fc11e6e01fe2e655ba4a82f33e81025c2baa3750
Peter Stackebrandt <p.stackebrandt@immowelt.de>
2019-07-02 14:36:33 +0200

    Solve 1 training unit while, if, let

--- Small Exercises/Functions and Lambdas/Functions/src/Task.kt
+++ Small Exercises/Functions and Lambdas/Functions/src/Task.kt
@@ -1,4 +1,12 @@
-//implement add() function here
+fun add(vararg numbers: Int): Int {
+    var sum = 0
+
+    for (number in numbers)
+        if (number % 2 == 0)
+            sum += number
+
+    return sum
+}
 
 /**
  * main method to play with
@@ -6,3 +14,4 @@
 fun main(args: Array<String>) {
     //println( add(1,2,3,4) )
 }
+
