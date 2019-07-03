ed6c2735638d6516358afb9c533374c72360a2e1
Peter Stackebrandt <p.stackebrandt@immowelt.de>
2019-07-03 12:05:30 +0200

    Solve 1 training unit generics

--- Small Exercises/Generics/Generic Class/src/Task.kt
+++ Small Exercises/Generics/Generic Class/src/Task.kt
@@ -1,11 +1,12 @@
-
-Generic Class Here
-
+class Transport<T>(val goods: T)
 
 class Dog
 class Cat
 
-fun catTransporter() //implement this function
+fun catTransporter(): Transport<Cat> {
+    val myCat = Cat()
+    return Transport(myCat)
+}
 
 
 fun main(args: Array<String>) {
