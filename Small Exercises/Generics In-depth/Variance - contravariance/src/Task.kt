a6f69f8959a1b86b053fbe46f4bcc94fc16f87bd
Peter Stackebrandt <p.stackebrandt@immowelt.de>
2019-07-08 16:55:01 +0200

    Solve 1 training unit extension unit contravariance

--- Small Exercises/Generics In-depth/Variance - contravariance/src/Task.kt
+++ Small Exercises/Generics In-depth/Variance - contravariance/src/Task.kt
@@ -1,15 +1,15 @@
 abstract class Dog(val name: String)
 class DanishDog(name: String) : Dog(name)
 
-open class Trainer<T> {
+open class Trainer<in T> {
 
     open fun train(target: T) {
         throw NotImplementedError()
     }
 
-    open fun trainedTarget(): T {
-        throw NotImplementedError()
-    }
+//    open fun trainedTarget(): T {
+//        throw NotImplementedError()
+//    }
 }
 
 class UseCase {
@@ -23,5 +23,6 @@ class UseCase {
         }
 
         //Assign the generic dog trainer to the danish dog trainer
+        danishDogTrainer = dogTrainer
     }
 }
